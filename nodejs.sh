087e4bc4911f3fdd7a8a5c475c0269223b7754e4
msrks <mshrrks@gmail.com>
2018-04-20 22:42:49 +0900

    nodejs

---
+++ nodejs.sh
@@ -0,0 +1,15 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+
+echo "" >> ~/.bash_profile
+echo "# nvm" >> ~/.bash_profile
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bash_profile
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> ~/.bash_profile
+echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> ~/.bash_profile
+
+nvm install v8.9.4
+nvm alias default v8.9.4
+
+brew install yarn --without-node
+npm install -g eslint
+npm install -g typescript
+npm install -g yo
